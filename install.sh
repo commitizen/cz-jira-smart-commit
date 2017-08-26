7546eac15681c3221332fc6067eca1ac3f07d463
Pietro Bongiovanni <pietro@fixico.nl>
2017-08-26 22:08:45 +0200

    Remove internal documentation url

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ echo "Installing JIRA smart commits"
 npm install -g cz-jira-smart-commit
 echo "Creating a global config file"
 echo '{ "path": "/usr/local/lib/node_modules/cz-jira-smart-commit/" }' > ~/.czrc
-echo "You can view documentation at the link: https://fixico.atlassian.net/wiki/pages/resumedraft.action?draftId=39425512"
