e2e1d234f4778890f47fc2c9ef017bceb319ba6e
Pietro Bongiovanni <pietro@fixico.nl>
2017-08-26 22:04:16 +0200

    Use npm package over repo in install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing Commitizen Globally"
 npm install -g commitizen
 echo "Installing JIRA smart commits"
-npm install -g https://github.com/pgoodjohn/cz-jira-smart-commit
+npm install -g cz-jira-smart-commit
 echo "Creating a global config file"
 echo '{ "path": "/usr/local/lib/node_modules/cz-jira-smart-commit/" }' > ~/.czrc
 echo "You can view documentation at the link: https://fixico.atlassian.net/wiki/pages/resumedraft.action?draftId=39425512"
